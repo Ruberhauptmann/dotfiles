d6f14419bc0202cc06388a4713c89628d8c4aeff
Tjark Sievers <git@tjarksievers.de>
2019-12-25 12:15:29 +0100

    Added some scripts

---
+++ dotfiles/scripts/tbfilters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers-1.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/catchall@tjarksievers'
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/mail@tjarksievers'
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers-1.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/catchall@tjarksievers.de'
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers-1.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/catchall@tjarksievers.de'
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers-1.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/catchall@tjarksievers.de'
+
+cp ~/.thunderbird/gcsms7ly.default-release/ImapMail/mail.tjarksievers-1.de/msgFilterRules.dat '~/Nextcloud/Wichtige Dateien/catchall@tjarksievers.de'
